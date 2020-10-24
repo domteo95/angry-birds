1fca1b09be7d0183fd51b7dc5422350ff184d5b1
Dominic Teo <dominicteo@cs.uchicago.edu>
2020-10-23 23:19:58 -0500

    implemented a change to eliminate the issue

--- AngryBirds/Views/BirdCell.swift
+++ AngryBirds/Views/BirdCell.swift
@@ -23,8 +23,10 @@ class BirdCell: UITableViewCell {
             DispatchQueue.global(qos: .userInitiated).async {
                 let birdImageData = NSData(contentsOf: URL(string: self.bird!.imageUrl)!)
                 DispatchQueue.main.async {
-                    self.birdImageView.image = UIImage(data: birdImageData as! Data)
-                    self.birdImageView.layer.cornerRadius = self.birdImageView.frame.width / 2
+                    if birdImageData != nil {
+                        self.birdImageView.image = UIImage(data: birdImageData as! Data)
+                        self.birdImageView.layer.cornerRadius = self.birdImageView.frame.width / 2
+                    }   
                 }
             }
         }
